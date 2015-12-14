71406c0ac81dd9c28b54e65ee85f5c900e4f4726
lowks <kianseong@gmail.com>
2015-12-14 19:50:15 +0800

    Adding description to setup.py

--- pysls/__init__.py
+++ pysls/__init__.py
@@ -1,6 +1,7 @@
 __version__ = '0.0.1'
 __author__ = 'Konstantin Kruglov'
 __email__ = 'kruglovk@gmail.com'
+__description__ = 'Simple light signal'
 
 from .sls import Redis
 from .config import Config
